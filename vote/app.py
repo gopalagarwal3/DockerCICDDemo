848cce4939a052788badfb9046283d857f910653
Gopal Agarwal <gagarwal@spglobal.com>
2019-04-17 21:15:12 +0530

    Marvel vs DC

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Marvel")
+option_b = os.getenv('OPTION_B', "DC")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
